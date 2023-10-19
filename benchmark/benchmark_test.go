e6a668882482999584a7b7ddbb8a12be280dbfd3
SivaprasadTamatam <cpp.sivaprasad@gmail.com>
2023-10-19 23:27:51 +0530

    Code coverage analysis changes

--- benchmark/benchmark_test.go
+++ benchmark/benchmark_test.go
@@ -2,6 +2,21 @@ package benchmark
 
 import "testing"
 
+func TestCalculateFibonacci(t *testing.T) {
+	result := CalculateFibonacci(3)
+
+	if result != 2 {
+		t.Errorf("Expected 2, but received %d ", result)
+	}
+}
+
+func TestSum(t *testing.T) {
+	result := Sum(10, 20, 30)
+
+	if result != 60 {
+		t.Errorf("Expected  Sum(10,20,30) = 60, but got %d", result)
+	}
+}
 func BenchmarkCalculateFibonacci(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		_ = CalculateFibonacci(10)
